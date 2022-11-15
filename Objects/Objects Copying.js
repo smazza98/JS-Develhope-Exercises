81db412ad56b7dcf92f3e694287f8335961686ee
sm <sm@mail.com>
2022-11-15 11:46:40 +0100

    Objects Copyingh End

---
+++ Objects/Objects Copying.js
@@ -0,0 +1,22 @@
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  };
+
+  /**When an object variable is copied, the reference is copied, but the object itself is not duplicated. */
+  
+  // const person2 = person1;
+  
+  /*Doing so, there’s still one object, but now with two variables that reference it.
+  We can use either variable to access the object and modify its contents: */
+  
+  // person2.firstNasme = "Simon";
+
+  // The proper way to do it would be to copy values from the provided object to a new object, like so...
+
+  const person2 = {...person1}; 
+  person2.firstName = "Simon";
+  
+  console.log(person1);
+  console.log(person2);
