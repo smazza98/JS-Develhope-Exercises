41664daf31fad831c302a5e171d1911bd83883a7
sm <sm@mail.com>
2022-11-23 12:32:07 +0100

    Ex3

---
+++ Array Methods Advanced/ex3.js
@@ -0,0 +1,21 @@
+const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+
+  function calculateAverageAge(arr) {
+    const age = arr.map(i => i.age);
+    return Math.round(age.reduce((a, b) => a + b) / age.length);
+  }
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
